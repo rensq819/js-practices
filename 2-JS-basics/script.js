137f27d926a3e43e75081d4b9ad3aac9a2486ed6
Siqi Ren <siqi.ren@gmail.com>
2020-06-21 16:20:45 -0400

    var, data type, var mutation, and type coercion

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,59 @@
-console.log('test test')
+/*****************************
+* Variables and data types
+*/
+
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3; // var name can start with _ or $, but not number
+var johnMark = 'John and MArk'; // var name cannot have '/'
+var if = 23; // error: cannot use js reserved as var name
+*/
+
+
+/*****************************
+* Variables mutation and type coercion
+*/
+
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+// age is logged as string together with firstName, becasue of type coercion (js automatically converts one type to another)
+console.log(firstName + ' ' + age);
+
+// define multiple vars here and assign values later
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+// boolean is logged as string as well due to type coercion, so does undefined
+console.log(`${firstName} is a ${age} year old ${job}. Is he married? ${isMarried}.`)
+
+// Variable Mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(`${firstName} is a ${age} year old ${job}. Is he married? ${isMarried}.`)
+
+var lastName = prompt('What is his last name?');
+
+console.log('Last name is: ',lastName);
+*/
+
+
