75bbfe5c9de0dea7df4d0c7f780934c627845c6b
Siqi Ren <siqi.ren@gmail.com>
2020-07-02 21:58:32 -0400

    function statement vs expression, array, challenge 3

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -135,7 +135,7 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 1. Store Mark's and John's mass and height in variables
 2. Calculate both their BMIs
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
-4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
 
 GOOD LUCK 😀
 */
@@ -288,7 +288,111 @@ if (scoreJohn > scoreMike) {
 */
 
 
+/*****************************
+* Function, Function Statements and Expressions
+*/
+/*
+// function declaration/statement
+function calculateAge(birthYear) {
+    return 2020 - birthYear;
+}
+console.log(calculateAge(1994))
 
+// Function declaration/statement
+// DOES NOT RETURN A VALUE IMMEDIATELY 
+// function whatDoYouDo(job, firstName) {}
+
+// function expression
+// RETURN A VALUE IMMEDIATELY 
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.'
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
+*/
+
+
+/*****************************
+* Arrays
+*/
+/*
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue'); // insert from tail
+john.unshift('Mr.'); // insert from head
+console.log(john);
+
+john.pop(); // remove from tail 
+john.shift(); // remove from head
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+*/
+
+
+/*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+*/
+/*
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = bills.map(bill => tipCalculator(bill));
+
+var finalValues = bills.map((bill, index) => bill + tips[index]);
+
+console.log(tips, finalValues);
+*/
 
 
 
