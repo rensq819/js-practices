fbb1789f453975bd6632c21455ada6ceef0b9da4
Siqi Ren <siqi.ren@gmail.com>
2020-07-04 15:56:11 -0400

    hoisting, scoping, this

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -585,7 +585,7 @@ var john = {
             if (bill < 50) {
                 percentage = .2;
             } else if (bill >= 50 && bill < 200) {
-                percentage = .15;
+                percentage = .15 ;
             } else {
                 percentage = .1;
             }
